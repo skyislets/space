d312d80b691ffca0148e87a2a39b833ea0e87609
skyislets <126557769+skyislets@users.noreply.github.com>
2023-04-24 17:38:46 +0800

    修复部分bug

---
+++ script/article.js
@@ -0,0 +1,10 @@
+function showTooltip() {
+    var tooltip = document.getElementById("tooltip");
+    tooltip.style.display = "block";
+  }
+  
+  function hideTooltip() {
+    var tooltip = document.getElementById("tooltip");
+    tooltip.style.display = "none";
+  }
+  
